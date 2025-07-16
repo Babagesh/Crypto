38493f09630b1ad79b5150266ce103170df863a5
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-07-16 18:50:34 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ export default function App()
 {
     const [list, setList] = useState(null);
 
+    const [filteredResults, setFilteredResults] = useState([])
+    const [searchInput, setSearchInput] = useState("")
+
     useEffect( () => {
         async function fetchAllCoinData() {
             const response = await fetch(`https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}`);
@@ -19,25 +22,69 @@ export default function App()
     }
     ,[])
 
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if(searchValue !== "")
+        {
+            const filteredData = Object.keys(list.Data).filter(
+                (item) => (
+                    Object.values(item).join("").toLowerCase().includes(searchValue.toLowerCase())
+                ))
+                setFilteredResults(filteredData);
+        }
+        else
+        {
+            setFilteredResults(Object.keys(list.Data))
+        }
+    }
+
   return (
     <div className = "whole-page">
         <h1>My Crypto List</h1>
+        <input 
+            type = "text"
+            placeholder = "Search..."
+            onChange = {(e) => searchItems(e.target.value)}
+        />
         <ul>
             {
-                (list && Object.entries(list.Data).filter(
-                    ([number, coin]) => 
-                    (
-                        coin['IsTrading'] && coin['Algorithm'] !== 'N/A' && coin['ProofType'] !== 'N/A'
+                searchInput.length > 0 ? 
+                (
+                    filteredResults.map((coin) => 
+                    {
+                        const coinData = list.Data[coin]
+                        if(coinData['IsTrading'] && coinData['Algorithm'] !== 'N/A' && coinData['ProofType'] !== 'N/A')
+                        {
+                            return (
+                            <CoinInfo 
+                                key = {coin}
+                                name = {coinData['FullName']}
+                                image = {coinData['ImageUrl']}
+                                symbol = {coinData['Symbol']}
+                           />)
+                        }
+                        return null;
+                    }
+                    )   
+                ) : 
+                (
+                        (list && Object.entries(list.Data).filter(
+                            ([coin, coinData]) => 
+                            (
+                                coinData['IsTrading'] && coinData['Algorithm'] !== 'N/A' && coinData['ProofType'] !== 'N/A'
+                            )
+                        )
+                        .slice(0,20)
+                        .map(([coin, coinData]) => 
+                            <CoinInfo
+                                key = {coin}
+                                name = {coinData['FullName']}
+                                image = {coinData['ImageUrl']}
+                                symbol = {coinData['Symbol']}
+                            />
+                        )
                     )
-                    
-                ).map(([number, coinData]) => 
-                    <CoinInfo 
-                        name = {coinData['FullName']}
-                        image = {coinData['ImageUrl']}
-                        symbol = {coinData['Symbol']}
-                    />
                 )
-            )
             }
         </ul>
     </div>
