9086030c2da217610e6a96e0cd87602b969283f5
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-07-22 01:28:57 -0400

    added page-router

---
+++ src/components/CoinDetail.jsx
@@ -0,0 +1,105 @@
+import {Component, useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+const API_KEY = import.meta.env.VITE_CRYPTO_ACCESS_KEY;
+
+
+const CoinDetail = () => {
+    const {symbol} = useParams();
+
+    const[fullDetails, setFullDetails] = useState(null);
+
+    useEffect(() =>
+    {
+        const coinDetails = async () => {
+            const details = await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?api_key=${API_KEY}&fsyms=${symbol}&tsyms=USD`)
+            const description = await fetch(`https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}&fsym=${symbol}`)
+
+            const detailsJson = await(details.json());
+            const descripJson = await(description.json());
+
+            console.log(detailsJson)
+            console.log(descripJson)
+            
+            setFullDetails({
+                numbers: detailsJson['DISPLAY'],
+                textData: descripJson['Data']
+            })
+        }
+        coinDetails().catch(console.error)
+    }, [symbol])
+
+    return fullDetails ? (
+        <div>
+            <h1> {fullDetails['textData'][symbol]['FullName']} </h1>
+            <img 
+                className = 'images'
+                src = {`https://www.cryptocompare.com${fullDetails['numbers'][symbol]['USD']['IMAGEURL']}`}
+                alt = {`Small icon for ${symbol} crypto coin`}
+            />
+            <div> {fullDetails['textData'][symbol]['Description']}</div>
+            <br></br>
+            <div>
+                This coin was built with the algorithm {" "}
+                {fullDetails['textData'][symbol]['Algorithm']}
+            </div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th> Launch Date</th>
+                        <td>{fullDetails['textData'][symbol]['AssetLaunchDate'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Website </th>
+                        <td>{fullDetails['textData'][symbol]['Website'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Whitepaper </th>
+                        <td>{fullDetails['textData'][symbol]['WhitePaper'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Monetary Symbol </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['FROMSYMBOL'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Market </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['MARKET'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Last Transaction </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['LASTUPDATE'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Last Transaction Value</th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['PRICE'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Volume </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['VOLUME24HOUR'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Today's Open Price </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['OPEN24HOUR'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Highest Price during the Day </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['HIGH24HOUR'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Lowest Price during the Day </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['LOW24HOUR'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Change from Previous Day </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['CHANGEPCT24HOUR'] || 'N/A'}</td>
+                    </tr>
+                    <tr>
+                        <th>Market Cap </th>
+                        <td>{fullDetails['numbers'][symbol]['USD']['MKTCAP'] || 'N/A'}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    ) : null;
+}
+
+export default CoinDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './routes/Layout.jsx'
+import DetailView from './routes/DetailView.jsx'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<Layout />}>
+        <Route index element = {<App />}/>
+        <Route path = '/coinDetails/:symbol' element={<DetailView />}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
